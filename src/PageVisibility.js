faa66600dab3f97f4f40136d7fb578bbcd8617ec
cristianbgp <cristian.granda.pastor@gmail.com>
2019-06-03 16:24:40 -0500

    Add little fix

--- src/PageVisibility.js
+++ src/PageVisibility.js
@@ -3,17 +3,16 @@ import React from "react";
 function PageVisibility({ children, render = children }) {
   const [status, setStatus] = React.useState(true);
 
-  function handleVisibility(event) {
-    if (document.hidden) {
-      console.log("Page is not visible");
-      setStatus(false);
-    } else {
-      console.log("Page is visible");
-      setStatus(true);
-    }
-  }
-
   React.useEffect(() => {
+    function handleVisibility(event) {
+      if (document.hidden) {
+        console.log("Page is not visible");
+        setStatus(false);
+      } else {
+        console.log("Page is visible");
+        setStatus(true);
+      }
+    }
     document.addEventListener("visibilitychange", handleVisibility);
     return () => {
       window.removeEventListener("visibilitychange", handleVisibility);
